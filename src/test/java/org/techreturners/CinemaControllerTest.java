4140df18d931bd61fe09d96f65cc154ab8602af9
dwright1989 <debbiewright@live.co.uk>
2023-02-22 13:52:29 +0000

    Additional tests added and passing for booking two tickets - cannot when the two seats are not together

--- src/test/java/org/techreturners/CinemaControllerTest.java
+++ src/test/java/org/techreturners/CinemaControllerTest.java
@@ -151,4 +151,54 @@ public class CinemaControllerTest {
         assertFalse(cinemaController.getCinema().getSeats().get(5).isAvailable());
         assertFalse(cinemaController.getCinema().getSeats().get(6).isAvailable());
     }
+
+    @Test
+    public void cannotBookWhenLastTwoSeatsSeparatedOnSameRow(){
+        List<Seat> seats = new ArrayList<Seat>();
+        seats.add(new Seat('A', 1, false));
+        seats.add(new Seat('A', 2, true));
+        seats.add(new Seat('A', 3, false));
+        seats.add(new Seat('A', 4, false));
+        seats.add(new Seat('A', 5, true));
+        seats.add(new Seat('B', 1, false));
+        seats.add(new Seat('B', 2, false));
+        seats.add(new Seat('B', 3, false));
+        seats.add(new Seat('B', 4, false));
+        seats.add(new Seat('B', 5, false));
+        seats.add(new Seat('C', 1, false));
+        seats.add(new Seat('C', 2, false));
+        seats.add(new Seat('C', 3, false));
+        seats.add(new Seat('C', 4, false));
+        seats.add(new Seat('C', 5, false));
+        CinemaController cinemaController = new CinemaController(seats);
+        assertFalse(cinemaController.bookTickets(2));
+        assertEquals(cinemaController.getCustomer().getSeats().size(), 0);
+        assertTrue(cinemaController.getCinema().getSeats().get(1).isAvailable());
+        assertTrue(cinemaController.getCinema().getSeats().get(4).isAvailable());
+    }
+
+    @Test
+    public void cannotBookWhenLastTwoSeatsSeparatedOnDifferentRows(){
+        List<Seat> seats = new ArrayList<Seat>();
+        seats.add(new Seat('A', 1, false));
+        seats.add(new Seat('A', 2, true));
+        seats.add(new Seat('A', 3, false));
+        seats.add(new Seat('A', 4, false));
+        seats.add(new Seat('A', 5, false));
+        seats.add(new Seat('B', 1, false));
+        seats.add(new Seat('B', 2, false));
+        seats.add(new Seat('B', 3, false));
+        seats.add(new Seat('B', 4, false));
+        seats.add(new Seat('B', 5, false));
+        seats.add(new Seat('C', 1, false));
+        seats.add(new Seat('C', 2, false));
+        seats.add(new Seat('C', 3, true));
+        seats.add(new Seat('C', 4, false));
+        seats.add(new Seat('C', 5, false));
+        CinemaController cinemaController = new CinemaController(seats);
+        assertFalse(cinemaController.bookTickets(2));
+        assertEquals(cinemaController.getCustomer().getSeats().size(), 0);
+        assertTrue(cinemaController.getCinema().getSeats().get(1).isAvailable());
+        assertTrue(cinemaController.getCinema().getSeats().get(12).isAvailable());
+    }
 }
