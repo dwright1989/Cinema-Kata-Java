03765735c8a0b9783df3206ffb5974ae15c0dd50
dwright1989 <debbiewright@live.co.uk>
2023-02-22 13:40:27 +0000

    Test passing for booking two seats when whole cinema available

--- src/main/java/org/techreturners/CinemaController.java
+++ src/main/java/org/techreturners/CinemaController.java
@@ -14,11 +14,26 @@ public class CinemaController {
     }
 
     public boolean bookTickets(int num){
-        for(Seat seat: cinema.getSeats()){
-            if(seat.isAvailable()){
-                seat.setAvailable(false);
-                customer.getSeats().add(seat);
-                return true;
+        List<Seat> seats = cinema.getSeats();
+        Seat nextSeat = seats.get(1);
+        for(int i=0; i<seats.size(); i++){
+            Seat currentSeat = seats.get(i);
+            if(seats.size()>i+1){
+                nextSeat = seats.get(1);
+            }
+            if(currentSeat.isAvailable()){
+                if(num>1 && !nextSeat.isAvailable()){
+                    return false;
+                }else{
+                    if(num>1){
+                        nextSeat.setAvailable(false);
+                        customer.getSeats().add(nextSeat);
+                    }
+                    currentSeat.setAvailable(false);
+                    customer.getSeats().add(currentSeat);
+                    return true;
+                }
+
             }
         }
         return false;
@@ -29,15 +44,8 @@ public class CinemaController {
         return cinema;
     }
 
-    public void setCinema(Cinema cinema) {
-        this.cinema = cinema;
-    }
-
     public Customer getCustomer() {
         return customer;
     }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
 }
